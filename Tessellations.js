641a1534bf591032fbcc6cf482d82c1b1cb4545e
CarlosAraki <carlosarak@gmail.com>
2022-11-18 22:34:24 +0000

    feat:tesselation test

--- Tessellations.js
+++ Tessellations.js
@@ -1,38 +1,64 @@
-import { SVG } from "https://cdn.skypack.dev/@svgdotjs/svg.js";
-import {
-  createVoronoiTessellation,
-  random
-} from "https://cdn.skypack.dev/@georgedoescode/generative-utils";
 
-const width = 196;
-const height = 196;
+var svg = d3.select("svg").on("touchmove mousemove", moved),
+width = +svg.attr("width"),
+height = +svg.attr("height");
 
-const svg = SVG().viewbox(0, 0, width, height);
+var sites = d3.range(100)
+.map(function(d) { return [Math.random() * width, Math.random() * height]; });
 
-svg.addTo("body");
+var voronoi = d3.voronoi()
+.extent([[-1, -1], [width + 1, height + 1]]);
 
-const points = [...Array(1024)].map(() => {
-  return {
-    x: random(0, width),
-    y: random(0, height)
-  };
-});
+var polygon = svg.append("g")
+.attr("class", "polygons")
+.selectAll("path")
+.data(voronoi.polygons(sites))
+.enter().append("path")
+.call(redrawPolygon);
 
-const tessellation = createVoronoiTessellation({
-  // The width of our canvas/drawing space
-  width,
-  // The height of our canvas/drawing space
-  height,
-  // The generating points we just created
-  points,
-  // How much we should "even out" our cell dimensions
-  relaxIterations: 6
-});
-console.log(tessellation)
-const debug = true;
+var link = svg.append("g")
+.attr("class", "links")
+.selectAll("line")
+.data(voronoi.links(sites))
+.enter().append("line")
+.call(redrawLink);
 
-tessellation.cells.forEach((cell) => {
-  if (debug) {
-    svg.polygon(cell.points).fill("none").stroke("#1D1934");
-  }
-});
+var site = svg.append("g")
+.attr("class", "sites")
+.selectAll("circle")
+.data(sites)
+.enter().append("circle")
+.attr("r", 5)
+.call(redrawSite);
+
+function moved() {
+sites[0] = d3.mouse(this);
+redraw();
+}
+
+function redraw() {
+var diagram = voronoi(sites);
+polygon = polygon.data(diagram.polygons()).call(redrawPolygon);
+link = link.data(diagram.links()), link.exit().remove();
+link = link.enter().append("line").merge(link).call(redrawLink);
+site = site.data(sites).call(redrawSite);
+}
+
+function redrawPolygon(polygon) {
+polygon
+  .attr("d", function(d) { return d ? "M" + d.join("L") + "Z" : null; });
+}
+
+function redrawLink(link) {
+link
+  .attr("x1", function(d) { return d.source[0]; })
+  .attr("y1", function(d) { return d.source[1]; })
+  .attr("x2", function(d) { return d.target[0]; })
+  .attr("y2", function(d) { return d.target[1]; });
+}
+
+function redrawSite(site) {
+site
+  .attr("cx", function(d) { return d[0]; })
+  .attr("cy", function(d) { return d[1]; });
+}
